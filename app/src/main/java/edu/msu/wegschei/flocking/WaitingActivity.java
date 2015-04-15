663bbae06db70702f7287218bc8601acf9cca332
benoitc2 <benoit.chris8@gmail.com>
2015-04-15 00:58:52 -0400

    basics of the 'waiting for player 2' activity, empty class for where that logic should go

---
+++ app/src/main/java/edu/msu/wegschei/flocking/WaitingActivity.java
@@ -0,0 +1,10 @@
+package edu.msu.wegschei.flocking;
+
+import android.support.v7.app.ActionBarActivity;
+
+public class WaitingActivity extends ActionBarActivity {
+
+    //This class is where we check with the server to see if there is a player 2 found.
+    //If there is, then this class takes in the name of the new player and exits to the
+    //starting game screen, with both names passed into it.
+}
